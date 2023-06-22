b01ec283901a35adf3a4d72381ee14756ff771d0
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-06-22 15:17:04 -0700

    Add password hash

--- server/models/user.py
+++ server/models/user.py
@@ -15,10 +15,21 @@ class User(db.Model, SerializerMixin):
 
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
-    # add password logic 
     
     def __repr__(self):
         return (
             f"User #{self.id}: {self.username}"
         )
+    
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hashes may not be viewed")
+    
+    @password_hash.setter
+    def password_hash(self, password): 
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+    
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, 
+                                            password.encode('utf-8'))
