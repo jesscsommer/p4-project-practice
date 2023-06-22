67ca04d75953dbea19ea2e885db47b09eff1809d
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-06-22 10:07:18 -0700

    Start tags

--- server/models/book_tag.py
+++ server/models/book_tag.py
@@ -1,1 +1,9 @@
 from __init__ import db, validates, SerializerMixin
+
+class BookTag(db.Model, SerializerMixin):
+    __tablename__ = "book_tags"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    book_id = db.Column(db.Integer, db.ForeignKey("books.id"))
+    tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"))
--- server/models/tag.py
+++ server/models/tag.py
@@ -1,1 +1,15 @@
 from __init__ import db, validates, SerializerMixin
+
+class Tag(db.Model, SerializerMixin):
+    __tablename__ = "tags"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __repr__(self):
+        return (
+            f"Tag #{self.id}: {self.name}"
+        )
